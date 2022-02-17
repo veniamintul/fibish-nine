a82ad366a0fe0c6a3d406dbffc071b84a6b7d3e3
veniamintul <tulupovvenya@yandex.ru>
2022-02-17 03:17:02 +0300

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,12 +1,48 @@
 let player;
+const playerContainer =$('.player');
+
+let eventsInit=function(){      //обработчики
+  $(".player__start").click(function(event){
+    event.preventDefault();
+
+    const btn =$(event.currentTarget);
+
+    if(playerContainer.hasClass('paused')){
+      playerContainer.removeClass("paused");
+      player.playVideo();
+    }else{
+      playerContainer.addClass("paused");
+      player.playVideo();
+    }
+
+    
+  })
+
+}
+
+
+
+
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('yt-player', {
-    height: '405',
+    height: '400',
     width: '660',
-    videoId: 'S9GMifozW74',
+    videoId: 'qDY-VmgTI4k',
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
+    },
+    playerVars: {
+      controls: 0,
+      disablekb: 0,//управление клавиатурой
+      showinfo: 0,
+      rel: 0,  //рекомендации
+      autoplay: 0,
+      modestbranding: 0
     }
   });
 }
+
+eventsInit();
+
+
